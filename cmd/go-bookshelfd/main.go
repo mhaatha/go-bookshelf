8aff1e25c149c75136dadb5133fa8bd5bb17fa7f
Hafidz Athaya <mhafidza528@gmail.com>
2025-09-22 21:19:05 +0800

    Log, Config, Database Initialization and write the author resources and routes

---
+++ cmd/go-bookshelfd/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"net/http"
+
+	"github.com/mhaatha/go-bookshelf/internal/config"
+	"github.com/mhaatha/go-bookshelf/internal/database"
+	"github.com/mhaatha/go-bookshelf/internal/handler"
+	"github.com/mhaatha/go-bookshelf/internal/repository"
+	"github.com/mhaatha/go-bookshelf/internal/router"
+	"github.com/mhaatha/go-bookshelf/internal/service"
+)
+
+func CreateAuthor(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Create author")
+}
+
+func main() {
+	// Log init
+	config.LogInit()
+
+	// Config init
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		slog.Error("godotenv fails to load .env file", "err", err)
+	}
+
+	// Database init
+	err = database.ConnectDB(cfg)
+	if err != nil {
+		slog.Error("failed connect to database", "err", err)
+	}
+	defer database.DB.Close()
+
+	// Main router
+	mux := http.NewServeMux()
+
+	// Author resources
+	authorRepository := repository.NewAuthorRepository()
+	authorService := service.NewAuthorService(authorRepository, database.DB)
+	authorHandler := handler.NewAuthorHandler(authorService)
+
+	// Author Router
+	router.AuthorRouter(authorHandler, mux)
+
+	// Server
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	slog.Info("starting server on :8080")
+	if err := server.ListenAndServe(); err != nil {
+		slog.Error("failed to start the server", "err", err)
+	}
+}

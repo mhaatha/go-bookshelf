b561c2803ee25ec53e590f91a15420a54e7b84e3
Hafidz Athaya <mhafidza528@gmail.com>
2025-09-22 21:23:21 +0800

    Add model for CreateAuthor response

---
+++ internal/model/web/author_response.go
@@ -0,0 +1,11 @@
+package web
+
+import "time"
+
+type CreateAuthorResponse struct {
+	Id          string    `json:"id"`
+	FullName    string    `json:"full_name"`
+	Nationality string    `json:"nationality"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}

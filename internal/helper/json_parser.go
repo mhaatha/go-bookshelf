606f1bf6aad5130303c1c2a41b874940c39e7e2a
Hafidz Athaya <mhafidza528@gmail.com>
2025-09-23 21:34:41 +0800

    Change WriteToResponseBody structure

--- internal/helper/json_parser.go
+++ internal/helper/json_parser.go
@@ -2,8 +2,10 @@ package helper
 
 import (
 	"encoding/json"
-	"log"
+	"log/slog"
 	"net/http"
+
+	"github.com/mhaatha/go-bookshelf/internal/model/web"
 )
 
 // ReadFromRequestBody reads the request body and stores it in the result parameter
@@ -18,12 +20,21 @@ func ReadFromRequestBody(r *http.Request, result interface{}) error {
 }
 
 // WriteToResponseBody writes the result parameter to the response body
-func WriteToResponseBody(w http.ResponseWriter, response interface{}) {
+func WriteToResponseBody(w http.ResponseWriter, statusCode int, response interface{}) {
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
 
 	encoder := json.NewEncoder(w)
 	if err := encoder.Encode(response); err != nil {
-		log.Fatal(err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		slog.Error("failed to encode response body", "err", err)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+
+		fallback := web.WebFailedResponse{
+			Errors: http.StatusText(http.StatusInternalServerError),
+		}
+
+		_ = json.NewEncoder(w).Encode(fallback)
 	}
 }

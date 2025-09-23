a9b86036c7aa0087ef183b304f9f5eb8225396ba
Hafidz Athaya <mhafidza528@gmail.com>
2025-09-23 10:08:28 +0800

    Add required validation to FullName and Nationality

--- internal/model/web/author_request.go
+++ internal/model/web/author_request.go
@@ -1,6 +1,6 @@
 package web
 
 type CreateAuthorRequest struct {
-	FullName    string `json:"full_name" validate:"min=3,max=255"`
-	Nationality string `json:"nationality" validate:"min=3,max=255"`
+	FullName    string `json:"full_name" validate:"required,min=3,max=255"`
+	Nationality string `json:"nationality" validate:"required,min=3,max=255"`
 }

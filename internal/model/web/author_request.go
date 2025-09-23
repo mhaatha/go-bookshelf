780cb4354d63cf28ec4d546fa32a953673f7ed07
Hafidz Athaya <mhafidza528@gmail.com>
2025-09-23 21:35:34 +0800

    Add validName and alpha tags

--- internal/model/web/author_request.go
+++ internal/model/web/author_request.go
@@ -1,6 +1,6 @@
 package web
 
 type CreateAuthorRequest struct {
-	FullName    string `json:"full_name" validate:"required,min=3,max=255,alpha"`
+	FullName    string `json:"full_name" validate:"required,min=3,max=255,validName"`
 	Nationality string `json:"nationality" validate:"required,min=3,max=255,alpha"`
 }

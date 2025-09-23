b47a4fa875f84d72d773936f12f101a5fdaed943
Hafidz Athaya <mhafidza528@gmail.com>
2025-09-23 11:14:59 +0800

    Helper function to take care of validation error

---
+++ internal/helper/validation_error.go
@@ -0,0 +1,34 @@
+package helper
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/go-playground/validator/v10"
+)
+
+func TranslateValidationErrors(err error) []map[string]string {
+	var validateErrs validator.ValidationErrors
+	if errors.As(err, &validateErrs) {
+		out := make([]map[string]string, 0, len(validateErrs))
+		for _, e := range validateErrs {
+			msg := ""
+			switch e.Tag() {
+			case "required":
+				msg = fmt.Sprintf("%s is required", e.Field())
+			case "min":
+				msg = fmt.Sprintf("%s must be at least %s characters", e.Field(), e.Param())
+			case "max":
+				msg = fmt.Sprintf("%s must be at most %s characters", e.Field(), e.Param())
+			default:
+				msg = fmt.Sprintf("%s is invalid", e.Field())
+			}
+			out = append(out, map[string]string{
+				"field":   e.Field(),
+				"message": msg,
+			})
+		}
+		return out
+	}
+	return nil
+}

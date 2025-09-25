4d1bcf10c32eee2e8a4ef747429068ef28baebfb
Hafidz Athaya <mhafidza528@gmail.com>
2025-09-25 16:05:55 +0800

    Wrapper error for service response

---
+++ internal/errors/service_error.go
@@ -0,0 +1,41 @@
+package errors
+
+import (
+	"errors"
+	"log/slog"
+	"net/http"
+
+	"github.com/mhaatha/go-bookshelf/internal/helper"
+	"github.com/mhaatha/go-bookshelf/internal/model/web"
+)
+
+func ResponseServiceErrorHandler(w http.ResponseWriter, err error, message string) {
+	// Validation error
+	validationErrs := TranslateValidationErrors(err)
+	if validationErrs != nil {
+		slog.Error("validation error", "err", err)
+
+		helper.WriteToResponseBody(w, http.StatusBadRequest, web.WebFailedResponse{
+			Errors: validationErrs,
+		})
+		return
+	}
+
+	// Custom error
+	var customErr *AppError
+	if errors.As(err, &customErr) {
+		slog.Error(message, "err", err)
+
+		helper.WriteToResponseBody(w, customErr.StatusCode, web.WebFailedResponse{
+			Errors: customErr.ErrAggregate,
+		})
+		return
+	}
+
+	// Unexpected error
+	slog.Error("error when calling CreateNewAuthor", "err", err)
+
+	helper.WriteToResponseBody(w, http.StatusInternalServerError, web.WebFailedResponse{
+		Errors: http.StatusText(http.StatusInternalServerError),
+	})
+}

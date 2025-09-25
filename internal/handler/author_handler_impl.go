a6c1980f10bca9e1b1cb9478c5ae30e126e3f218
Hafidz Athaya <mhafidza528@gmail.com>
2025-09-25 16:05:38 +0800

    Wrap errors when calling functions to particular wrapper error functions

--- internal/handler/author_handler_impl.go
+++ internal/handler/author_handler_impl.go
@@ -1,7 +1,6 @@
 package handler
 
 import (
-	"errors"
 	"log/slog"
 	"net/http"
 
@@ -26,43 +25,14 @@ func (handler *AuthorHandlerImpl) Create(w http.ResponseWriter, r *http.Request)
 	authorRequest := web.CreateAuthorRequest{}
 	err := helper.ReadFromRequestBody(r, &authorRequest)
 	if err != nil {
-		slog.Error("failed read JSON from request body", "err", err)
+		appError.RequestJSONErrorHandler(w, err)
 		return
 	}
 
 	// Call the service
 	authorResponse, err := handler.AuthorService.CreateNewAuthor(r.Context(), authorRequest)
 	if err != nil {
-		// Check if it's a validation error
-		validationErrs := appError.TranslateValidationErrors(err)
-
-		if validationErrs != nil {
-			slog.Error("validation error", "err", err)
-
-			helper.WriteToResponseBody(w, http.StatusBadRequest, web.WebFailedResponse{
-				Errors: validationErrs,
-			})
-			return
-		}
-
-		// Check if it's a custom error
-		var customErr *appError.AppError
-
-		if errors.As(err, &customErr) {
-			slog.Error("failed to create author", "err", err)
-
-			helper.WriteToResponseBody(w, customErr.StatusCode, web.WebFailedResponse{
-				Errors: customErr.ErrAggregate,
-			})
-			return
-		}
-
-		// If it's not a validation error, then it's an unexpected error
-		slog.Error("error when calling CreateNewAuthor", "err", err)
-
-		helper.WriteToResponseBody(w, http.StatusInternalServerError, web.WebFailedResponse{
-			Errors: http.StatusText(http.StatusInternalServerError),
-		})
+		appError.ResponseServiceErrorHandler(w, err, "failed to create new author")
 		return
 	}
 

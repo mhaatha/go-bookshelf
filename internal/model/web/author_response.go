e04a7061a418972e0ce5ee30075a75a9d34946b7
Hafidz Athaya <mhafidza528@gmail.com>
2025-09-25 22:05:23 +0800

    Response model for get authors

--- internal/model/web/author_response.go
+++ internal/model/web/author_response.go
@@ -9,3 +9,11 @@ type CreateAuthorResponse struct {
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
 }
+
+type GetAuthorResponse struct {
+	Id          string    `json:"id"`
+	FullName    string    `json:"full_name"`
+	Nationality string    `json:"nationality"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}

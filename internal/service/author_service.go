3bcde17bb8ef51397cbb77c88d6e7d5f8ac0b514
Hafidz Athaya <mhafidza528@gmail.com>
2025-09-26 14:54:08 +0800

    Add UpdateAuthorById service method

--- internal/service/author_service.go
+++ internal/service/author_service.go
@@ -10,4 +10,5 @@ type AuthorService interface {
 	CreateNewAuthor(ctx context.Context, request web.CreateAuthorRequest) (web.CreateAuthorResponse, error)
 	GetAllAuthors(ctx context.Context, queris web.QueryParamsGetAuthors) ([]web.GetAuthorResponse, error)
 	GetAuthorById(ctx context.Context, pathValues web.PathParamsGetAuthor) (web.GetAuthorResponse, error)
+	UpdateAuthorById(ctx context.Context, pathValues web.PathParamsGetAuthor, request web.UpdateAuthorRequest) (web.UpdateAuthorResponse, error)
 }

7aada6d5852510a5b7ac65cfdbb38aefe5a909b9
Hafidz Athaya <mhafidza528@gmail.com>
2025-09-26 14:53:51 +0800

    Add update author by id route

--- internal/router/author_router.go
+++ internal/router/author_router.go
@@ -10,4 +10,5 @@ func AuthorRouter(handler handler.AuthorHandler, mux *http.ServeMux) {
 	mux.HandleFunc("POST /api/v1/authors", handler.Create)
 	mux.HandleFunc("GET /api/v1/authors", handler.GetAll)
 	mux.HandleFunc("GET /api/v1/authors/{id}", handler.GetById)
+	mux.HandleFunc("PUT /api/v1/authors/{id}", handler.UpdateById)
 }

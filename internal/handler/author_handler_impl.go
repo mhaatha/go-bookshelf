3e47ba6cda422e8911823d1273507fbd7f0ced46
Hafidz Athaya <mhafidza528@gmail.com>
2025-09-26 14:05:09 +0800

    Implement GetById handler method

--- internal/handler/author_handler_impl.go
+++ internal/handler/author_handler_impl.go
@@ -13,6 +13,8 @@ import (
 const (
 	queryFullName    = "full_name"
 	queryNationality = "nationality"
+
+	wildcardId = "id"
 )
 
 func NewAuthorHandler(authorService service.AuthorService) AuthorHandler {
@@ -80,5 +82,31 @@ func (handler *AuthorHandlerImpl) GetAll(w http.ResponseWriter, r *http.Request)
 	helper.WriteToResponseBody(w, http.StatusOK, web.WebSuccessResponse{
 		Message: "Sucess get all authors",
 		Data:    authorsResponse,
+	})
+}
+
+func (handler *AuthorHandlerImpl) GetById(w http.ResponseWriter, r *http.Request) {
+	pathValue := web.PathParamsGetAuthor{
+		Id: r.PathValue(wildcardId),
+	}
+
+	// Call the service
+	authorResponse, err := handler.AuthorService.GetAuthorById(r.Context(), pathValue)
+	if err != nil {
+		appError.ResponseServiceErrorHandler(w, err, "failed to get author by id")
+		return
+	}
+
+	// Log the info
+	slog.Info("request handled",
+		"method", r.Method,
+		"endpoint", r.URL,
+		"status", http.StatusOK,
+	)
+
+	// Write and send the response
+	helper.WriteToResponseBody(w, http.StatusOK, web.WebSuccessResponse{
+		Message: "Sucess get author",
+		Data:    authorResponse,
 	})
 }

8d6fc17736560518d53a195f245d1493790d380c
Hafidz Athaya <mhafidza528@gmail.com>
2025-09-26 14:04:59 +0800

    Add GetById method

--- internal/handler/author_handler.go
+++ internal/handler/author_handler.go
@@ -5,4 +5,5 @@ import "net/http"
 type AuthorHandler interface {
 	Create(w http.ResponseWriter, r *http.Request)
 	GetAll(w http.ResponseWriter, r *http.Request)
+	GetById(w http.ResponseWriter, r *http.Request)
 }

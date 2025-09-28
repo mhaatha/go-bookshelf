cee116446cf603294796973ce1a6c1f2da261737
Hafidz Athaya <mhafidza528@gmail.com>
2025-09-29 06:51:11 +0800

    MinIO init function

---
+++ internal/config/minio.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"log/slog"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+)
+
+func MinIOInit(cfg *Config) (*minio.Client, error) {
+	endpoint := cfg.MinIOEndpoint
+	accessKeyId := cfg.MinIOAccessKeyId
+	secretAccessKey := cfg.MinIOSecretAccessKey
+
+	minioClient, err := minio.New(endpoint, &minio.Options{
+		Creds: credentials.NewStaticV4(accessKeyId, secretAccessKey, ""),
+	})
+	if err != nil {
+		slog.Error("failed to create MinIO client", "err", err)
+		return nil, err
+	}
+
+	return minioClient, nil
+}

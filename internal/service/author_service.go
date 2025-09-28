1e2eb034b7995d902aad9376f965b98ec16cbf40
Hafidz Athaya <mhafidza528@gmail.com>
2025-09-29 07:14:13 +0800

    Change pathValue data type

--- internal/service/author_service.go
+++ internal/service/author_service.go
@@ -10,6 +10,6 @@ type AuthorService interface {
 	CreateNewAuthor(ctx context.Context, request web.CreateAuthorRequest) (web.CreateAuthorResponse, error)
 	GetAllAuthors(ctx context.Context, queris web.QueryParamsGetAuthors) ([]web.GetAuthorResponse, error)
 	GetAuthorById(ctx context.Context, pathValues web.PathParamsGetAuthor) (web.GetAuthorResponse, error)
-	UpdateAuthorById(ctx context.Context, pathValues web.PathParamsGetAuthor, request web.UpdateAuthorRequest) (web.UpdateAuthorResponse, error)
-	DeleteAuthorById(ctx context.Context, pathValues web.PathParamsGetAuthor) error
+	UpdateAuthorById(ctx context.Context, pathValues web.PathParamsUpdateAuthor, request web.UpdateAuthorRequest) (web.UpdateAuthorResponse, error)
+	DeleteAuthorById(ctx context.Context, pathValues web.PathParamsDeleteAuthor) error
 }

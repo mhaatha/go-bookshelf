7bdedf2371208fd9cdc6bddf26748409c92ee251
Hafidz Athaya <mhafidza528@gmail.com>
2025-09-29 14:53:50 +0800

    Book domain

--- internal/model/domain/book.go
+++ internal/model/domain/book.go
@@ -3,13 +3,13 @@ package domain
 import "time"
 
 type Book struct {
-	Id           string    `json:"id"`
-	Name         string    `json:"name"`
-	TotalPage    int       `json:"total_page"`
-	AuthorId     string    `json:"author_id"`
-	PhotoURL     string    `json:"photo_url,omitempty"`
-	Status       string    `json:"status"`
-	DateComplete time.Time `json:"date_complete,omitempty"`
-	CreatedAt    time.Time `json:"created_at"`
-	UpdatedAt    time.Time `json:"updated_at"`
+	Id            string    `json:"id"`
+	Name          string    `json:"name"`
+	TotalPage     int       `json:"total_page"`
+	AuthorId      string    `json:"author_id"`
+	PhotoURL      string    `json:"photo_url,omitempty"`
+	Status        string    `json:"status"`
+	CompletedDate time.Time `json:"completed_date,omitempty"`
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
 }

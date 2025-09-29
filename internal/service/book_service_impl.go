b92645290c3968f4969ac31d75695452e9170d3b
Hafidz Athaya <mhafidza528@gmail.com>
2025-09-29 14:52:58 +0800

    Implement CreateNewBook service method

---
+++ internal/service/book_service_impl.go
@@ -0,0 +1,98 @@
+package service
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/jackc/pgx/v5/pgxpool"
+	appError "github.com/mhaatha/go-bookshelf/internal/errors"
+	"github.com/mhaatha/go-bookshelf/internal/helper"
+	"github.com/mhaatha/go-bookshelf/internal/model/domain"
+	"github.com/mhaatha/go-bookshelf/internal/model/web"
+	"github.com/mhaatha/go-bookshelf/internal/repository"
+)
+
+func NewBookService(bookRepository repository.BookRepository, authorService AuthorService, db *pgxpool.Pool, validate *validator.Validate) BookService {
+	return &BookServiceImpl{
+		BookRepository: bookRepository,
+		AuthorService:  authorService,
+		DB:             db,
+		Validate:       validate,
+	}
+}
+
+type BookServiceImpl struct {
+	BookRepository repository.BookRepository
+	AuthorService  AuthorService
+	DB             *pgxpool.Pool
+	Validate       *validator.Validate
+}
+
+func (service *BookServiceImpl) CreateNewBook(ctx context.Context, request web.CreateBookRequest) (web.CreateBookResponse, error) {
+	// Validate request body
+	err := service.Validate.Struct(request)
+	if err != nil {
+		return web.CreateBookResponse{}, err
+	}
+
+	// Open transaction
+	tx, err := service.DB.Begin(ctx)
+	if err != nil {
+		return web.CreateBookResponse{}, err
+	}
+	defer helper.CommitOrRollback(ctx, tx)
+
+	// errAggregate aggregates errors from user bad request
+	errAggregate := []appError.ErrAggregate{}
+
+	// Check if author_id exists
+	_, err = service.AuthorService.GetAuthorById(ctx, web.PathParamsGetAuthor{Id: request.AuthorId})
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			errAggregate = append(errAggregate, appError.ErrAggregate{
+				Field:   "author_id",
+				Message: fmt.Sprintf("author with id '%v' is not found", request.AuthorId),
+			})
+		} else {
+			return web.CreateBookResponse{}, err
+		}
+	}
+
+	// Check if there is a book with the same name and the same author_id
+	err = service.BookRepository.CheckByNameAndAuthorId(ctx, tx, request.Name, request.AuthorId)
+	if err != nil {
+		errAggregate = append(errAggregate, appError.ErrAggregate{
+			Field:   "name",
+			Message: fmt.Sprintf("%v with author_id '%v' is already exists", request.Name, request.AuthorId),
+		})
+	}
+
+	if len(errAggregate) != 0 {
+		return web.CreateBookResponse{}, appError.NewAppError(
+			http.StatusBadRequest,
+			errAggregate,
+			nil,
+		)
+	}
+
+	book := domain.Book{
+		Name:          request.Name,
+		TotalPage:     request.TotalPage,
+		AuthorId:      request.AuthorId,
+		PhotoURL:      request.PhotoURL,
+		Status:        request.Status,
+		CompletedDate: request.CompletedDate,
+	}
+
+	// Call repository
+	book, err = service.BookRepository.Save(ctx, tx, book)
+	if err != nil {
+		return web.CreateBookResponse{}, err
+	}
+
+	return helper.ToCreateBookResponse(book), nil
+}

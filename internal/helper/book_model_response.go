c020609dd04f8d4a2686a999a027b4c3b1ab63c3
Hafidz Athaya <mhafidza528@gmail.com>
2025-09-30 09:50:27 +0800

    Add book responses helper functions

--- internal/helper/book_model_response.go
+++ internal/helper/book_model_response.go
@@ -18,3 +18,25 @@ func ToCreateBookResponse(book domain.Book) web.CreateBookResponse {
 		UpdatedAt:     book.UpdatedAt,
 	}
 }
+
+func ToGetBookResponse(book domain.Book) web.GetBookResponse {
+	return web.GetBookResponse{
+		Id:            book.Id,
+		Name:          book.Name,
+		TotalPage:     book.TotalPage,
+		AuthorId:      book.AuthorId,
+		PhotoURL:      book.PhotoURL,
+		Status:        book.Status,
+		CompletedDate: book.CompletedDate,
+		CreatedAt:     book.CreatedAt,
+		UpdatedAt:     book.UpdatedAt,
+	}
+}
+
+func ToGetBooksResponse(books []domain.Book) []web.GetBookResponse {
+	var bookResponses []web.GetBookResponse
+	for _, book := range books {
+		bookResponses = append(bookResponses, ToGetBookResponse(book))
+	}
+	return bookResponses
+}

5d4911897e239acc3171d4fcf94f2e28a52cd156
Hafidz Athaya <mhafidza528@gmail.com>
2025-09-30 10:28:47 +0800

    Add UpdateBookById service method

--- internal/service/book_service.go
+++ internal/service/book_service.go
@@ -10,4 +10,5 @@ type BookService interface {
 	CreateNewBook(ctx context.Context, request web.CreateBookRequest) (web.CreateBookResponse, error)
 	GetAllBooks(ctx context.Context, queries web.QueryParamsGetBooks) ([]web.GetBookResponse, error)
 	GetBookById(ctx context.Context, pathValues web.PathParamsGetBook) (web.GetBookResponse, error)
+	UpdateBookById(ctx context.Context, pathValues web.PathParamsUpdateBook, request web.UpdateBookRequest) (web.UpdateBookResponse, error)
 }

6f6440f36b439962a10ca362130b53a2c7b91cbb
Hafidz Athaya <mhafidza528@gmail.com>
2025-09-30 09:51:03 +0800

    Book response model

--- internal/model/web/book_response.go
+++ internal/model/web/book_response.go
@@ -13,3 +13,15 @@ type CreateBookResponse struct {
 	CreatedAt     time.Time `json:"created_at"`
 	UpdatedAt     time.Time `json:"updated_at"`
 }
+
+type GetBookResponse struct {
+	Id            string    `json:"id"`
+	Name          string    `json:"name"`
+	TotalPage     int       `json:"total_page"`
+	AuthorId      string    `json:"author_id"`
+	PhotoURL      string    `json:"photo_url"`
+	Status        string    `json:"status"`
+	CompletedDate time.Time `json:"completed_date"`
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
+}

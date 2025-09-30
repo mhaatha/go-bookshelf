da09d0b09c85a1ed738bfe328b440118e3a06d9e
Hafidz Athaya <mhafidza528@gmail.com>
2025-09-30 09:09:12 +0800

    Change CompletedDate type to string

--- internal/model/web/book_request.go
+++ internal/model/web/book_request.go
@@ -1,12 +1,10 @@
 package web
 
-import "time"
-
 type CreateBookRequest struct {
-	Name          string    `json:"name" validate:"required,min=3,max=255"`
-	TotalPage     int       `json:"total_page" validate:"required,number,min=1,max=12000"`
-	AuthorId      string    `json:"author_id" validate:"required,uuid"`
-	PhotoURL      string    `json:"photo_url" validate:"omitempty"`
-	Status        string    `json:"status" validate:"required,bookStatus"`
-	CompletedDate time.Time `json:"completed_date" validate:"omitempty,datetime=2006-01-02"`
+	Name          string `json:"name" validate:"required,min=3,max=255"`
+	TotalPage     int    `json:"total_page" validate:"required,number,min=1,max=12000"`
+	AuthorId      string `json:"author_id" validate:"required,uuid"`
+	PhotoURL      string `json:"photo_url" validate:"omitempty"`
+	Status        string `json:"status" validate:"required,bookStatus"`
+	CompletedDate string `json:"completed_date" validate:"omitempty,datetime=2006-01-02"`
 }

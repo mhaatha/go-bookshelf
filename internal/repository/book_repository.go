1c95902eefdbb0c6fe1af6581cdc57fd6dc39251
Hafidz Athaya <mhafidza528@gmail.com>
2025-09-30 10:02:52 +0800

    Add FindById repository method

--- internal/repository/book_repository.go
+++ internal/repository/book_repository.go
@@ -11,4 +11,5 @@ type BookRepository interface {
 	Save(ctx context.Context, tx pgx.Tx, book domain.Book) (domain.Book, error)
 	CheckByNameAndAuthorId(ctx context.Context, tx pgx.Tx, name, authorId string) error
 	FindAll(ctx context.Context, tx pgx.Tx, name, status, author_name string) ([]domain.Book, error)
+	FindById(ctx context.Context, tx pgx.Tx, bookId string) (domain.Book, error)
 }

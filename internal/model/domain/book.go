521d57b5cb699a7e796e751413e65eefa2fd3c38
Hafidz Athaya <mhafidza528@gmail.com>
2025-09-30 23:04:44 +0800

    Add BookWithURL for get purpose

--- internal/model/domain/book.go
+++ internal/model/domain/book.go
@@ -13,3 +13,15 @@ type Book struct {
 	CreatedAt     time.Time `json:"created_at"`
 	UpdatedAt     time.Time `json:"updated_at"`
 }
+
+type BookWithURL struct {
+	Id            string    `json:"id"`
+	Name          string    `json:"name"`
+	TotalPage     int       `json:"total_page"`
+	AuthorId      string    `json:"author_id"`
+	PhotoURL      string    `json:"photo_url,omitempty"`
+	Status        string    `json:"status"`
+	CompletedDate time.Time `json:"completed_date,omitempty"`
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
+}

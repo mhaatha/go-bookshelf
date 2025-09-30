f68122f320fb7443dc81193badcfca045d1194ea
Hafidz Athaya <mhafidza528@gmail.com>
2025-09-30 10:03:47 +0800

    Add GET /api/v1/books/{id} route

--- internal/router/book_router.go
+++ internal/router/book_router.go
@@ -9,4 +9,5 @@ import (
 func BookRouter(handler handler.BookHandler, mux *http.ServeMux) {
 	mux.HandleFunc("POST /api/v1/books", handler.Create)
 	mux.HandleFunc("GET /api/v1/books", handler.GetAll)
+	mux.HandleFunc("GET /api/v1/books/{id}", handler.GetById)
 }

fcedb24415880fac0cf0df86ce63e22dae717f9a
Hafidz Athaya <mhafidza528@gmail.com>
2025-10-23 17:06:17 +0800

    refactor: delete tx with pgx.Tx type to accomplish loose coupling

--- internal/repository/book_repository.go
+++ internal/repository/book_repository.go
@@ -3,15 +3,14 @@ package repository
 import (
 	"context"
 
-	"github.com/jackc/pgx/v5"
 	"github.com/mhaatha/go-bookshelf/internal/model/domain"
 )
 
 type BookRepository interface {
-	Save(ctx context.Context, tx pgx.Tx, book domain.Book) (domain.Book, error)
-	CheckByNameAndAuthorId(ctx context.Context, tx pgx.Tx, name, authorId string) error
-	FindAll(ctx context.Context, tx pgx.Tx, name, status, author_name string) ([]domain.Book, error)
-	FindById(ctx context.Context, tx pgx.Tx, bookId string) (domain.Book, error)
-	Update(ctx context.Context, tx pgx.Tx, bookId string, book domain.Book) (domain.Book, error)
-	Delete(ctx context.Context, tx pgx.Tx, bookId string) error
+	Save(ctx context.Context, book domain.Book) (domain.Book, error)
+	CheckByNameAndAuthorId(ctx context.Context, name, authorId string) error
+	FindAll(ctx context.Context, name, status, author_name string) ([]domain.Book, error)
+	FindById(ctx context.Context, bookId string) (domain.Book, error)
+	Update(ctx context.Context, bookId string, book domain.Book) (domain.Book, error)
+	Delete(ctx context.Context, bookId string) error
 }

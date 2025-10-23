6a0d2dfb72c812a5ae657cdd1c4ef18b35675d96
Hafidz Athaya <mhafidza528@gmail.com>
2025-10-23 17:04:46 +0800

    refactor: delete tx with pgx.Tx type to accomplish loose coupling

--- internal/repository/author_repository.go
+++ internal/repository/author_repository.go
@@ -3,15 +3,14 @@ package repository
 import (
 	"context"
 
-	"github.com/jackc/pgx/v5"
 	"github.com/mhaatha/go-bookshelf/internal/model/domain"
 )
 
 type AuthorRepository interface {
 	Save(ctx context.Context, author domain.Author) (domain.Author, error)
-	CheckByFullName(ctx context.Context, tx pgx.Tx, fullName string) error
-	FindAll(ctx context.Context, tx pgx.Tx, fullName, nationality string) ([]domain.Author, error)
-	FindById(ctx context.Context, tx pgx.Tx, authorId string) (domain.Author, error)
-	Update(ctx context.Context, tx pgx.Tx, authorId string, author domain.Author) (domain.Author, error)
-	Delete(ctx context.Context, tx pgx.Tx, authorId string) error
+	CheckByFullName(ctx context.Context, fullName string) error
+	FindAll(ctx context.Context, fullName, nationality string) ([]domain.Author, error)
+	FindById(ctx context.Context, authorId string) (domain.Author, error)
+	Update(ctx context.Context, authorId string, author domain.Author) (domain.Author, error)
+	Delete(ctx context.Context, authorId string) error
 }

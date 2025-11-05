48cef1d4a6d86b783e6feac4523d5b10a0bab2df
Hafidz Athaya <mhafidza528@gmail.com>
2025-11-05 13:28:20 +0800

    add: handler test create new book with invalid status

--- internal/handler/book_handler_test.go
+++ internal/handler/book_handler_test.go
@@ -522,4 +522,88 @@ func TestBookCreateHandler(t *testing.T) {
 			})
 		}
 	})
+
+	t.Run("create book with invalid status", func(t *testing.T) {
+		cases := []struct {
+			Name        string
+			BookRequest web.CreateBookRequest
+			ErrField    string
+			ErrMessage  string
+		}{
+			{
+				Name: "required",
+				BookRequest: web.CreateBookRequest{
+					Name:          "Laut Bercerita",
+					TotalPage:     379,
+					AuthorId:      "c512ae16-5f33-4a3c-a1e1-977bd5a20af3",
+					PhotoKey:      "ac0a9b20-2e77-4905-a665-3006763d1934.jpg",
+					CompletedDate: "2025-09-29",
+				},
+				ErrField:   "status",
+				ErrMessage: "status is required",
+			},
+			{
+				Name: "invalid book status",
+				BookRequest: web.CreateBookRequest{
+					Name:          "Laut Bercerita",
+					TotalPage:     379,
+					AuthorId:      "c512ae16-5f33-4a3c-a1e1-977bd5a20af3",
+					PhotoKey:      "ac0a9b20-2e77-4905-a665-3006763d1934.jpg",
+					Status:        "InvalidStatus",
+					CompletedDate: "2025-09-29",
+				},
+				ErrField:   "status",
+				ErrMessage: "the valid value for this field are only 'completed', 'reading', and 'plan_to_read'",
+			},
+		}
+
+		validate := config.ValidatorInit()
+		for _, c := range cases {
+			t.Run(c.Name, func(t *testing.T) {
+				bookRequest := c.BookRequest
+				expectedServiceError := validate.Struct(bookRequest)
+
+				mockService := &MockBookService{
+					MockError: expectedServiceError,
+				}
+
+				handler := NewBookHandler(mockService)
+
+				req := httptest.NewRequest(http.MethodPost, "/api/v1/books", ToJSON(bookRequest))
+				res := httptest.NewRecorder()
+
+				handler.Create(res, req)
+
+				// Check status code
+				if res.Code != http.StatusBadRequest {
+					t.Errorf("expected status code of %d but got %d", http.StatusBadRequest, res.Code)
+				}
+
+				// Get the actual response
+				var actualResponseBody web.WebFailedResponse
+				err := json.NewDecoder(res.Body).Decode(&actualResponseBody)
+				if err != nil {
+					t.Fatalf("error when parsing res body: %v", err)
+				}
+
+				errorList, ok := actualResponseBody.Errors.([]interface{})
+				if ok {
+					val, ok := errorList[0].(map[string]interface{})
+					if ok {
+						if val["field"] != c.ErrField {
+							t.Errorf("expected error field is %s but got %s", c.ErrField, val["field"])
+						}
+
+						if val["message"] != c.ErrMessage {
+							t.Errorf("expected error message is %s but got %s", c.ErrMessage, val["message"])
+						}
+					} else {
+						t.Error("val should be true but got false")
+					}
+				} else {
+					t.Error("errorList should be true but got false")
+				}
+			})
+		}
+	})
 }

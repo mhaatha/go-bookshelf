4a3ff908f30133adf531632965d28505d645da2b
Hafidz Athaya <mhafidza528@gmail.com>
2025-12-20 17:37:48 +0800

    add: createdAt and updatedAt field in create user response

--- internal/model/web/auth_response.go
+++ internal/model/web/auth_response.go
@@ -1,8 +1,12 @@
 package web
 
+import "time"
+
 type CreateUserResponse struct {
-	FullName string `json:"full_name"`
-	Email    string `json:"email"`
+	FullName  string    `json:"full_name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 type LoginResponse struct {
